f5cbfcc54206b606d788c6715b5a8cce0c6d302d
kumar-abhi001 <kumar.abhi.ok@gmail.com>
2024-03-30 00:18:30 +0530

    Created file structure

---
+++ server/db/conn.mjs
@@ -0,0 +1,13 @@
+import { MongoClient } from "mongodb";
+
+const connectionString = process.env.ATLAS_URI || "";
+const client = new MongoClient(connectionString);
+let conn;
+try {
+    conn = await client.connect();
+} catch (e) {
+    console.error(e);
+}
+let db = conn.db("get-my-news");
+
+export default db;
