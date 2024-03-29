5aa26c3a8b28b81b223f9c2cb1ed877f7435a02f
Abhay2342 <choudharyabhay2342@gmail.com>
2024-03-30 03:05:13 +0530

    buttons added

--- client/src/components/HeroSection/HeroSection.jsx
+++ client/src/components/HeroSection/HeroSection.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography, Button } from "@mui/material";
+
 import "./HeroSection.css";
 
 const HeroSection = ({}) => {
@@ -16,17 +17,41 @@ const HeroSection = ({}) => {
     >
       <Grid container spacing={0} paddingX={"20px"}>
         <Grid item xs={12} sm={12}>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-      <Typography variant="bannerH1" align="center">
-        CareerCompass
-      </Typography>
-      <Typography variant="tagLine" align="center">
-        "Connect Talent With Opportunity"
-      </Typography>
-    </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Typography variant="bannerH1" align="center">
+              CareerCompass
+            </Typography>
+            <Typography variant="tagLine" align="center">
+              "Connect Talent With Opportunity"
+            </Typography>
+          </div>
         </Grid>
-        <Grid item xs={12} sm={12}>
-          {/* Empty column */}
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={12}
+          spacing={3}
+          container
+          justifyContent="center"
+        >
+          <Grid item>
+            <Button variant="header" size="large">
+              Login
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="header" size="large">
+              SIGN UP
+            </Button>
+          </Grid>
         </Grid>
       </Grid>
     </Box>
