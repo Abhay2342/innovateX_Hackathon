570cf197477462fdb66fb72e8e4721e3fcd01a8e
Abhay2342 <choudharyabhay2342@gmail.com>
2024-03-30 10:17:36 +0530

    update jobs page

--- client/src/pages/jobs/JobsPage.jsx
+++ client/src/pages/jobs/JobsPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useContext } from "react";
-import img from "../../assets/preview.png";
+import JobListingCard from "../../components/JobListingCard";
 
 import {
   Container,
@@ -16,6 +16,7 @@ import {
 import GoogleIcon from "../../assets/google.svg";
 import TwitterIcon from "../../assets/twitter.svg";
 import LinkedInIcon from "../../assets/linkedin.svg";
+import FilterSidebar from "../../components/JobsFilter";
 import { useNavigate } from "react-router-dom"; // Import useNavigate
 // import { useUser } from "../../components/UserContext";
 const JobsPage = () => {
@@ -26,21 +27,17 @@ const JobsPage = () => {
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false); // State to manage loading spinner
 
-  // Handler for login button click
-  const handleSignUpClick = () => {
-    navigate("/signup");
-  };
+  const [filters, setFilters] = useState({
+    company: "",
+    location: "",
+    skills: "",
+    position: "",
+  });
 
-  const handleLogoClick = () => {
-    navigate("/");
-  };
-
-  const handleUsernameChange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
+  // Handler for applying filters
+  const handleFilterSubmit = () => {
+    // Implement filter logic here to filter job listings based on filter values
+    console.log("Applying filters:", filters);
   };
 
   const handleLoginSubmit = async (event) => {
@@ -105,7 +102,6 @@ const JobsPage = () => {
           handleFilterSubmit={handleFilterSubmit}
         />
       </Grid>
-      {/* Right Side with Login Form */}
       <Divider
         orientation="vertical"
         sx={{ marginLeft: "30px", borderRightWidth: 2 }}
