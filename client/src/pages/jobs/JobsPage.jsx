4ce9570cc0d48b6cf7f9b192a50f1f96dd24d3ce
Abhay2342 <choudharyabhay2342@gmail.com>
2024-03-30 13:58:56 +0530

    ignore file

--- client/src/pages/jobs/JobsPage.jsx
+++ client/src/pages/jobs/JobsPage.jsx
@@ -1,32 +1,12 @@
-import React, { useState } from "react";
-import { useContext } from "react";
+import React, { useState, useEffect } from "react";
+import { Grid, Divider } from "@mui/material";
 import JobListingCard from "../../components/JobListingCard";
-
-import {
-  Container,
-  Grid,
-  Paper,
-  TextField,
-  Button,
-  Typography,
-  Divider,
-  Link,
-  CircularProgress, // Import CircularProgress for the loading spinner
-} from "@mui/material";
-import GoogleIcon from "../../assets/google.svg";
-import TwitterIcon from "../../assets/twitter.svg";
-import LinkedInIcon from "../../assets/linkedin.svg";
 import FilterSidebar from "../../components/JobsFilter";
-import { useNavigate } from "react-router-dom"; // Import useNavigate
-// import { useUser } from "../../components/UserContext";
+import { useNavigate } from "react-router-dom";
+
 const JobsPage = () => {
   const navigate = useNavigate();
-  // const { user, loginUser } = useUser(); // Destructure user and loginUser from useUser
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false); // State to manage loading spinner
-
+  const [data, setData] = useState([]);
   const [filters, setFilters] = useState({
     company: "",
     location: "",
@@ -40,52 +20,30 @@ const JobsPage = () => {
     console.log("Applying filters:", filters);
   };
 
-  const handleLoginSubmit = async (event) => {
-    event.preventDefault();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/jobs`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
 
-    // try {
-    //   setLoading(true);
+        if (response.ok) {
+          const fetchedData = await response.json();
+          console.log(fetchedData);
+          setData(fetchedData);
+        } else {
+          console.error("Failed to fetch jobs");
+        }
+      } catch (error) {
+        console.error("Error during fetch:", error);
+      }
+    };
 
-    //   const response = await fetch(
-    //     // "https://innovate-x-hackathon.vercel.app/login",
-    //     "http://localhost:3000/login",
-    //     {
-    //       method: "POST",
-    //       headers: {
-    //         "Content-Type": "application/json",
-    //       },
-    //       body: JSON.stringify({
-    //         email,
-    //         password,
-    //       }),
-    //     }
-    //   );
-
-    //   if (response.ok) {
-    //     const userData = await response.json();
-    //     console.log("Logged IN");
-    //     // Update user context with email and other user data
-    //     // loginUser(userData);
-    //     // console.log(userData[0]);
-    //     // console.log(JSON.stringify(userData));
-    //     // localStorage.setItem("user", JSON.stringify(userData[0]));
-    //     // localStorage.setItem(
-    //     //   "userCollection",
-    //     //   JSON.stringify(userData[1].collection)
-    //     // );
-    //     // localStorage.setItem("isLoggedIn", "true");
-
-    //     // setLoading(false);
-    //     // navigate("/profile-settings");
-    //   } else {
-    //     console.error("Login failed");
-    //     setLoading(false);
-    //   }
-    // } catch (error) {
-    //   console.error("Error during login:", error);
-    //   setLoading(false);
-    // }
-  };
+    fetchData();
+  }, []);
 
   return (
     <Grid
@@ -96,7 +54,6 @@ const JobsPage = () => {
       style={{ height: "80vh" }}
     >
       <Grid item xs={3} alignSelf={"flex-start"} sx={{ marginX: "30px" }}>
-        {" "}
         <FilterSidebar
           filters={filters}
           setFilters={setFilters}
@@ -119,63 +76,17 @@ const JobsPage = () => {
         alignSelf={"flex-start"}
         sx={{ overflowY: "auto", paddingX: "30px" }}
       >
-        <Grid item xs={6}>
-          <JobListingCard
-            jobTitle="Frontend Developer"
-            company="ABC Company"
-            location="New York, NY"
-            description="We are seeking a talented Frontend Developer to join our team. If you have experience with ReactJS and a passion for creating beautiful user interfaces, apply now!"
-            applyLink="https://example.com/apply"
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <JobListingCard
-            jobTitle="Frontend Developer"
-            company="ABC Company"
-            location="New York, NY"
-            description="We are seeking a talented Frontend Developer to join our team. If you have experience with ReactJS and a passion for creating beautiful user interfaces, apply now!"
-            applyLink="https://example.com/apply"
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <JobListingCard
-            jobTitle="Backend Engineer"
-            company="XYZ Corporation"
-            location="San Francisco, CA"
-            description="Join our team as a Backend Engineer and work on cutting-edge technologies. We're looking for someone with strong experience in Node.js and databases."
-            applyLink="https://example.com/apply"
-          />
-        </Grid>
-
-        <Grid item xs={6}>
-          <JobListingCard
-            jobTitle="Backend Engineer"
-            company="XYZ Corporation"
-            location="San Francisco, CA"
-            description="Join our team as a Backend Engineer and work on cutting-edge technologies. We're looking for someone with strong experience in Node.js and databases."
-            applyLink="https://example.com/apply"
-          />
-        </Grid>
-
-        <Grid item xs={6}>
-          <JobListingCard
-            jobTitle="Backend Engineer"
-            company="XYZ Corporation"
-            location="San Francisco, CA"
-            description="Join our team as a Backend Engineer and work on cutting-edge technologies. We're looking for someone with strong experience in Node.js and databases."
-            applyLink="https://example.com/apply"
-          />
-        </Grid>
-
-        <Grid item xs={6}>
-          <JobListingCard
-            jobTitle="Backend Engineer"
-            company="XYZ Corporation"
-            location="San Francisco, CA"
-            description="Join our team as a Backend Engineer and work on cutting-edge technologies. We're looking for someone with strong experience in Node.js and databases."
-            applyLink="https://example.com/apply"
-          />
-        </Grid>
+        {data.map((job, index) => (
+          <Grid item xs={6} key={index}>
+            <JobListingCard
+              jobTitle={job.position}
+              company={job.company}
+              location={job.location}
+              description={job.description}
+              applyLink={job.applyLink}
+            />
+          </Grid>
+        ))}
       </Grid>
     </Grid>
   );
